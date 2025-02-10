a2e0476e63c32cba6aec7876f4361f059869f806
samirvithlani <samir.vithlani83955@gmail.com>
2025-02-10 11:49:50 +0530

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import { ArrayDemo1 } from './components/ArrayDemo1'
 import { ArrayDemo2 } from './components/ArrayDemo2'
 import { ArrayDemo3 } from './components/ArrayDemo3'
 import { ArrayDemo4 } from './components/ArrayDemo4'
+import { UseStateDemo1 } from './components/UseStateDemo1'
+import { UseStateDemo2 } from './components/UseStateDemo2'
 
 
 function App() {
@@ -16,9 +18,11 @@ function App() {
 
   return (
     <div>
+      {/* <UseStateDemo1></UseStateDemo1> */}
+      <UseStateDemo2></UseStateDemo2>
       {/* <ArrayDemo1></ArrayDemo1> */}
       {/* <ArrayDemo2></ArrayDemo2> */}
-      <ArrayDemo4></ArrayDemo4>
+      {/* <ArrayDemo4></ArrayDemo4> */}
       {/* <Header></Header>
       <Content></Content>
       <Footer></Footer> */}
